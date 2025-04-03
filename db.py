debe81ad6ad2a14aa0f2299b955864067eecc660
Saziso Buthelezi <sazisoshenge1@gmail.com>
2025-04-03 21:13:39 +0200

    db made, and the login form

---
+++ db.py
@@ -0,0 +1,25 @@
+import  sqlite3
+class Database():
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS students(studentID INTEGER  PRIMARY KEY AUTOINCREMENT, name text, surname text, program text, gender text, phone text)")
+        self.conn.commit()
+
+    def fetch(self):
+        self.cur.execute("SELECT * FROM students")
+        rows = self.cur.fetchall()
+        return rows
+
+    def insert(self, studentID, name, surname, program, gender, phone):
+        self.cur.execute("INSERT INTO students VALUES(NULL, ?, ?,? ,?,?)", (studentID,name, surname, program, gender, phone))
+        self.conn.commit()
+    def update(self, studentID, name, surname, program, gender, phone):
+        self.cur.execute("UPDATE students SET name=?, surname=?, program=?, gender=?, phone=? WHERE studentID=?", (name, surname, program, gender, phone, studentID))
+        self.conn.commit()
+    def delete(self, studentID):
+        self.cur.execute("DELETE FROM students WHERE studentID=? ", (studentID,))
+        self.conn.commit()
+    def __del__(self):
+        self.conn.close()
+db = Database("studentDB.db")
--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+import tkinter as tk
+from db import  Database
+class Login():
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Student Management Application")
+        self.root.geometry("350x250")
+        self.create_widgets()
 
+
+    def create_widgets(self):
+        self.usernameTxt = tk.StringVar()
+        self.usernameLbl = tk.Label(
+            self.root, text="Username", pady=20, padx=10, font=("bold", 14))
+        self.usernameLbl.grid(column=0, row=0, sticky="ew")
+        self.username_Entry = tk.Entry(self.root, textvariable=self.usernameTxt)
+        self.username_Entry.grid(column=2, row=0)
+
+        self.password = tk.StringVar()
+        self.passwordLbl = tk.Label(
+            self.root, text="Password", padx=10, pady=10 ,font=("bold", 14))
+        self.passwordLbl.grid(column=0, row=1, sticky="ew")
+        self.password_Entry = tk.Entry(self.root, textvariable=self.password)
+        self.password_Entry.grid(column=2, row=1)
+
+        self.loginBtn = tk.Button(self.root, text="Log in", width=15, padx=10)
+        self.loginBtn.grid(column=2, row=3)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Login(root)
+    root.mainloop()
